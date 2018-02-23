c90c60d9d663a81e5ebe6fe9bbf9a994f8adc42c
Tuoris <tuoris.dev@gmail.com>
2018-02-23 19:41:24 +0200

    Refactor: Database interaction

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-import sqlite3
 from flask import Flask, jsonify, render_template, request, redirect, url_for, g
 from .validation import validate_form, validate_id
+from .storing import get_record, get_records, save_record, update_record, delete_record
+from .storing import init_db
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -16,7 +17,7 @@ app.config.update(dict(
 
 @app.route('/')
 def index():
-    records = get_records()
+    records = get_records(app)
     return render_template('index.html', job_list=records)
 
 
@@ -26,16 +27,16 @@ def add_record():
     valid_form = validate_form(form)
 
     if valid_form:
-        save_record(valid_form)
+        save_record(valid_form, app)
 
     return redirect(url_for('index'))
 
 
 @app.route('/delete_record', methods=['POST'])
-def delete_record():
+def handle_delete_record():
     form = request.form
     record_id = form['id']
-    delete_db_record(record_id)
+    delete_record(record_id, app)
     return ''
 
 
@@ -50,7 +51,7 @@ def fetch_record():
     if not record_id:
         return jsonify({})
 
-    record = get_record(record_id)
+    record = get_record(record_id, app)
     return jsonify(record)
 
 
@@ -62,105 +63,15 @@ def edit_record():
     valid_form['record_id'] = valid_id
 
     if valid_form and valid_id:
-        update_record(valid_form)
+        update_record(valid_form, app)
 
     return redirect(url_for('index'))
 
 
-def save_record(record):
-    db = get_db()
-    db.execute(
-        'insert into job_records (job_title, company, job_url, score, salary) values (?, ?, ?, ?, ?)',
-        [record['job_title'], record['company'],
-            record['job_url'], record['score'], record['salary']]
-    )
-    db.commit()
-    db.close()
-
-
-def update_record(record):
-    db = get_db()
-    db.execute(
-        'update job_records set job_title = (?), company = (?), '
-        'job_url = (?), score = (?), salary = (?) where id==(?)',
-        [record['job_title'], record['company'],
-            record['job_url'], record['score'], record['salary'], record['record_id']]
-    )
-    db.commit()
-    db.close()
-
-
-def get_records():
-    db = get_db()
-    cur = db.execute(
-        'select * from job_records'
-    )
-    records = cur.fetchall()
-    db.close()
-    records = [dict(r) for r in records]
-    return records
-
-
-def get_record(record_id):
-    db = get_db()
-    cur = db.execute(
-        'select * from job_records where id==(?)',
-        [record_id]
-    )
-    records = cur.fetchall()
-    db.close()
-    if records:
-        record = dict(records[0])
-    else:
-        record = {}
-    return record
-
-
-def delete_db_record(record_id):
-    db = get_db()
-    db.execute(
-        'delete from job_records where id==(?)',
-        [record_id]
-    )
-    db.commit()
-    db.close()
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, 'sqlite_db', None)
-    if db is not None:
-        db.close()
-
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
 @app.cli.command('initdb')
 def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
+    init_db(app)
+    print('Database initialized.')
 
 
 def main():
---
+++ storing.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+
+def save_record(record, app):
+    db = get_db(app)
+    db.execute(
+        'insert into job_records (job_title, company, job_url, score, salary) values (?, ?, ?, ?, ?)',
+        [record['job_title'], record['company'],
+            record['job_url'], record['score'], record['salary']]
+    )
+    db.commit()
+    db.close()
+
+
+def update_record(record, app):
+    db = get_db(app)
+    db.execute(
+        'update job_records set job_title = (?), company = (?), '
+        'job_url = (?), score = (?), salary = (?) where id==(?)',
+        [record['job_title'], record['company'],
+            record['job_url'], record['score'], record['salary'], record['record_id']]
+    )
+    db.commit()
+    db.close()
+
+
+def get_records(app):
+    db = get_db(app)
+    cur = db.execute(
+        'select * from job_records'
+    )
+    records = cur.fetchall()
+    db.close()
+    records = [dict(r) for r in records]
+    return records
+
+
+def get_record(record_id, app):
+    db = get_db(app)
+    cur = db.execute(
+        'select * from job_records where id==(?)',
+        [record_id]
+    )
+    records = cur.fetchall()
+    db.close()
+    if records:
+        record = dict(records[0])
+    else:
+        record = {}
+    return record
+
+
+def delete_record(record_id, app):
+    db = get_db(app)
+    db.execute(
+        'delete from job_records where id==(?)',
+        [record_id]
+    )
+    db.commit()
+    db.close()
+
+
+def connect_db(app):
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db(app):
+    return connect_db(app)
+
+
+def init_db(app):
+    db = get_db(app)
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
