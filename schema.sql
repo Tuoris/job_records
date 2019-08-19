5cba2afd7f8cc70792d05ac76d58b19d32d2fcc4
tuoris <tuoris.dev@gmail.com>
2019-08-19 12:23:29 +0300

    Add users table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,14 @@
-CREATE TABLE job_records (
+CREATE TABLE IF NOT EXISTS job_records (
   id SERIAL PRIMARY KEY,
   job_title TEXT NOT NULL,
   company TEXT NOT NULL,
   job_url TEXT NOT NULL,
   score INTEGER NOT NULL,
   salary REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL
 );
