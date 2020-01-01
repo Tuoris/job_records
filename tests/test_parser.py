f9fd7baa4edc15b54a11857d5dff2e591fbb4eef
Tuoris <tuoris.dev@gmail.com>
2020-01-01 13:47:23 +0200

    Add parser tests for m.rabota.ua site

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from unittest.mock import Mock, patch
 
 from tests import TEST_DATA_DIR
-from info_utils import work_ua_info
+from info_utils import work_ua_info, m_rabota_info
 
 
 def fake_request_data_with_local_file(url):
@@ -35,3 +35,20 @@ class WorkUAParserTest(TestCase):
         self.assertEqual(info['company'], 'Quintagroup')
         self.assertNotIn('salary', info)
 
+
+class MRabotaUAParserTest(TestCase):
+    @patch('info_utils.get_content', Mock(side_effect=fake_request_data_with_local_file))
+    def test_parses_m_rabota_ua_with_all_data(self):
+        info = m_rabota_info('https://m.rabota.ua/vacancy/view/7641600')
+
+        self.assertEqual(info['job_title'], 'Java Developer')
+        self.assertEqual(info['company'], 'Skillsquare')
+        self.assertEqual(info['salary'], 80000)
+
+    @patch('info_utils.get_content', Mock(side_effect=fake_request_data_with_local_file))
+    def test_parses_m_rabota_ua_with_no_salary(self):
+        info = m_rabota_info('https://m.rabota.ua/vacancy/view/7841428')
+
+        self.assertEqual(info['job_title'], 'Computer Vision Engineer')
+        self.assertEqual(info['company'], 'SEBALE')
+        self.assertNotIn('salary', info)
