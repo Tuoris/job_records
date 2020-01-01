f3e66c9bf67917c923c1405789176631424f16aa
Tuoris <tuoris.dev@gmail.com>
2020-01-01 13:47:34 +0200

    Fix parser for redesigned m.rabota.ua site

--- info_utils.py
+++ info_utils.py
@@ -25,30 +25,20 @@ def m_rabota_info(url):
         return info
 
     soup = BeautifulSoup(content, 'html.parser')
-    paper_blocks = soup.find_all(class_='paper-block')
 
-    top_block = _extract_first(paper_blocks)
-    if top_block:
-        salary_block = _extract_first(
-            top_block.find_all(class_='salary')
-        )
-        if salary_block:
-            salary = salary_block.text.split()[0]
+    title_block = soup.select_one('h1')
+    if title_block:
+        info['job_title'] = title_block.text
+
+    company_block = soup.select_one('a[href*="/company/view"] span')
+    if company_block:
+        info['company'] = company_block.text
+
+    salary_block = soup.select_one('div[class*="leading-20"]')
+    if salary_block:
+        salary, _, currency = salary_block.text.partition(' ')
+        if salary:
             info['salary'] = validate_salary(salary)
-
-        title_block = _extract_first(
-            top_block.find_all(class_='name')
-        )
-        if title_block:
-            info['job_title'] = title_block.text
-
-    second_block = _extract_first(paper_blocks[1:])
-    if second_block:
-        company_block = _extract_first(
-            second_block.find_all(class_='company-name')
-        )
-        if company_block:
-            info['company'] = company_block.text
 
     return info
 
